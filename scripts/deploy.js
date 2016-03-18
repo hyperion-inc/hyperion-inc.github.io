b05802bfb7a3c71d8cff83f6cc9b102b774191b0
Alex Lu <alex@hyperionindustries.io>
2016-03-18 00:55:33 -0700

    create deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,10 @@
+import ghpages from "gh-pages"
+import path from "path"
+
+ghpages.publish(path.join(__dirname, "..", "dist"), {
+  branch: "master",
+  user: {
+    name: "npm gh-pages",
+    email: "gh-pages@localhost",
+  },
+}, () => console.log("deploy complete!"))
